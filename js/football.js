97cc7fe53589617830bb7653d30148cb3b29bb0c
Leonel Medel Ilhuicatzi <medelill@iut1.local>
2017-11-30 16:00:13 +0100

    Update js and football page

--- js/football.js
+++ js/football.js
@@ -9,11 +9,8 @@ $.ajax({
 		for (var i =0; i<ligues.length; i++)
 		{
 			
-			$("#ligues").append("<button type='button' class='list-group-item list-group-item-action'"+
+			$("#ligues").append("<button type='button' class='list-group-item list-group-item-success' "+
 			 "onclick=\"appelTeams('"+ligues[i]._links.teams.href+"', '"+ligues[i].caption+"')\">"+ligues[i].caption+"</button>");
-			//console.log(ligues[i].caption);//+"			URL : "+ligues[i]._links.teams.href);
-			//console.log(ligues);
-
 		}
 	}
 });
@@ -43,15 +40,13 @@ function appelTeams(team, ligue)
 				console.log(name +"coucou"+ img);
 				if(img != null)
 				{
-				$("#teams").append("<div class='col-md-3' id = 'team"+e+"' onclick=\"appelPlayers('"+teams.teams[e]._links.players.href+"', '"+teams.teams[e].name+"')\"><img id='img' src='"+img+"' alt='images/bfoot.png'><div><h1>"+teams.teams[e].name+"</h1></div></div>");
-				//$("#logo").attr('src',teams.teams[e].crestUrl);	
+					$("#teams").append("<div class='col-md-3' id = 'team"+e+"' onclick=\"appelPlayers('"+teams.teams[e]._links.players.href+"', '"+teams.teams[e].name+"')\"><img id='img' src='"+img+"' ><div><h1>"+teams.teams[e].name+"</h1></div></div>");
 				}
 				else
 				{
 					$("#teams").append("<div class='col-md-3' id = 'team"+e+"' onclick=\"appelPlayers('"+teams.teams[e]._links.players.href+"', '"+teams.teams[e].name+"')\"><img id='img' src='images/bfoot.png' alt='images/bfoot.png'><div><h1>"+teams.teams[e].name+"</h1></div></div>");
 				}	
 			}
-			
 		}
 	});
 }
@@ -79,22 +74,15 @@ function appelPlayers(players, team){
 					var jer = play.players[e].jerseyNumber;
 					var pay = play.players[e].nationality
 					var date = play.players[e].dateOfBirth;
-
-					console.log("Joueur : "+ nom);
-					console.log("Position "+ pos);
-					console.log("Jersey "+ jer);
-					console.log("Pays de naicensse "+ pay);
-					console.log("birthDay "+ date );
-					console.log();
-
-					$("#players").append("<li onclick=\"creeModal('"+nom+"' '"+pos+"' '"+jer+"' '"+pay+"' '"+date+"')\" id='player"+e+"'>"+play.players[e].name+"</li>");
+					
+					$("#players").append("<li onclick=\"creeModal('"+nom+"', '"+pos+"', '"+jer+"', '"+pay+"', '"+date+"')\","+
+						" data-toggle='modal' data-target='#exampleModalLong'id='player"+e+"'>"+play.players[e].name+"</li>");
 				}
 			}
 			else
 			{
 				alert("Rien trouvé");
 			}
-			
 		}
 	});
 }
@@ -110,28 +98,16 @@ function creeModal(nom, pos, jer, pay, date)
 	console.log();
 	
 	//Pour eviter la concatenation de contenu 
-	//$( ".modal-body" ).empty();
+	$( ".modal-body" ).empty();
 
-	//Creation du Modal d'un personnage
-	/*
+	//Creation du Modal d'un joueur
 	$(".modal-body").append(
-		"<ul class='list-group info'>"+
-			"<li class='list-group-item'> <h1 class = 'nom'>"+nom+"</h1></li>"+
-			"<li class='list-group-item list-group-item-dark'> <h5>Mass: </h5> </li>"+
-			"<li class='list-group-item'>"+mass+"</li>"+
-			"<li class='list-group-item list-group-item-dark'><h5>Couleur des yeux:</h5> </li>"+
-			"<li class='list-group-item'>"+eyes+"</li>"+
-			"<li class='list-group-item list-group-item-dark'><h5>Taille:</h5></li>"+
-			"<li class='list-group-item'>"+he+"</li>"+
-			"<li class='list-group-item list-group-item-dark'><h5>Couleur des cheveux:</h5></li>"+
-			"<li class='list-group-item'>"+hair+"</li>"+
-			"<li class='list-group-item list-group-item-dark'><h5>Peau:</h5></li>"+
-			"<li class='list-group-item'>"+skin+"</li>"+
-			"<li class='list-group-item list-group-item-dark'><h5>Joyeux anniversaire:</h5></li>"+
-			"<li class='list-group-item'>"+birth+"</li>"+
-			"<li class='list-group-item list-group-item-dark'><h5>Genre:</h5></li>"+
-			"<li class='list-group-item'>"+gender+"</li>"+
-			"<li class='list-group-item list-group-item-dark'><h5>Films:</h5>"+
-			"</li>"+
-		"</ul>");*/
+	"<ul class='list-group info'>"+
+		"<li class='list-group-item'> <h2 class = 'nom'>"+nom+"</h2></li>"+
+		"<li class='list-group-item list-group-item-dark'> <p>Position de terrain: "+pos+"</p> </li>"+
+		"<li class='list-group-item list-group-item-dark'> <p>Numero de maillot: "+jer+"</p> </li>"+
+		"<li class='list-group-item list-group-item-dark'> <p>Pays de naissance: "+pay+"</p> </li>"+
+		"<li class='list-group-item list-group-item-dark'> <p>Date de naissance: "+date+"</p> </li>"+
+	"</ul>");
+
 }
